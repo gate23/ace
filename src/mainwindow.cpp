e6fcb422f9944473125cff51e9823f8c0dd1e32c
Andrew Young <younga25@students.wwu.edu>
2014-05-02 17:24:39 -0700

    moved

---
+++ src/mainwindow.cpp
@@ -0,0 +1,20 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+/* NOTE: I renamed trainer.ui to mainwindow.ui for this. */
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+
+    /* Status label being added to status bar */
+    statusLabel = new QLabel(this);
+    statusLabel->setText("Status label");
+    ui->statusbar->addPermanentWidget(statusLabel);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
