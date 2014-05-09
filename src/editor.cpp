c82f2feeaa8fe87d8f594fe5d8e6f0f4eca9d207
Sam <dunlaps@students.wwu.edu>
2014-05-09 15:13:58 -0700

    sfasd

--- src/editor.cpp
+++ src/editor.cpp
@@ -6,9 +6,17 @@ editor::editor(QWidget *parent) :
     ui(new Ui::editor)
 {
     ui->setupUi(this);
+
+    connect(ui->actionMain_Menu,SIGNAL(clicked())),this,SLOT(actionMain_MenuClicked());
 }
 
 editor::~editor()
 {
     delete ui;
+}
+
+void editor::actionMain_MeduClicked()
+{
+    mw.show();
+    this->hide;
 }
--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-#include "ui_mainwindow.h"
 #include "editor.h"
 #include "trainer.h"
 #include <QApplication>
