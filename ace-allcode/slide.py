77884721dc7653da7b67e4e71ba6bf9846b4ac09
Joshua Charles Minor <minorj3@quest-OptiPlex-9010>
2014-10-08 14:59:34 -0700

    Slide is now the base class of SlideGen.

---
+++ ace-allcode/slide.py
@@ -0,0 +1,42 @@
+"""
+slide.py
+
+Superclass for slide display widgets
+"""
+
+from PyQt4 import QtGui, QtCore
+
+class Slide(QtGui.QWidget):
+    VIEW_WIDTH,VIEW_HEIGHT = 540,540
+    
+    scale_size = 15
+
+    def __init__(self,parent):
+        super(Slide, self).__init__(parent)
+        
+        self.scene = QtGui.QGraphicsScene(  QtCore.QRectF(
+                                                0,0,
+                                                Slide.VIEW_WIDTH,
+                                                Slide.VIEW_HEIGHT),
+                                            self)
+
+        self.view = QtGui.QGraphicsView(self.scene)
+        self.view.setParent(self)
+        self.view.setMinimumSize(QtCore.QSize(Slide.VIEW_WIDTH, Slide.VIEW_HEIGHT))
+        self.view.setSizePolicy(    QtGui.QSizePolicy.MinimumExpanding,
+                                    QtGui.QSizePolicy.MinimumExpanding)        
+
+        layout = QtGui.QVBoxLayout()
+        layout.addWidget(self.view)
+        self.setLayout(layout)
+
+        #Load and set view background
+        self.bg_texture = QtGui.QPixmap('background.jpg')
+        background = QtGui.QBrush(self.bg_texture)
+        self.scene.setBackgroundBrush(background)
+        
+        #Load and scale the texture for the algae cells
+        self.texture = QtGui.QPixmap('aphanothece1.png')
+        self.texture = self.texture.scaled( QtCore.QSize(Slide.scale_size, Slide.scale_size) )
+        
+       
