49d487be0282d88d921f23d97cacb36b522b2357
josh <minorj3@students.wwu.edu>
2014-11-25 12:48:14 -0800

    Cleaned generator.py, added bash script: type ./go instead of 'python ...' Efficiencyls

--- ace-allcode/generator.py
+++ ace-allcode/generator.py
@@ -13,6 +13,7 @@ from random import triangular
 
 class Generator(QtGui.QWidget):
     SLIDE_WIDTH,SLIDE_HEIGHT = 540,540
+    LOW_POW2, HIGH_POW2 = 3,10 #defines upper and lower ranges
     
     def __init__(self, parent):
         super(Generator,self).__init__(parent)
@@ -84,22 +85,20 @@ class Generator(QtGui.QWidget):
     def initNumCellsLayout(self):
         widget = QtGui.QWidget(self)
         self.guess_group = QtGui.QButtonGroup(widget)
-        self.r0 = QtGui.QRadioButton("8-15")
-        self.guess_group.addButton(self.r0)
-        self.r1 = QtGui.QRadioButton("16-31")
-        self.guess_group.addButton(self.r1)
-        self.r2 = QtGui.QRadioButton("32-63")
-        self.guess_group.addButton(self.r2)
-        self.r3 = QtGui.QRadioButton("64-127")
-        self.guess_group.addButton(self.r3)
-        self.r4 = QtGui.QRadioButton("128-255")
-        self.guess_group.addButton(self.r4)
-        self.r5 = QtGui.QRadioButton("256-511")
-        self.guess_group.addButton(self.r5)
-        self.r6 = QtGui.QRadioButton("512-1023")
-        self.guess_group.addButton(self.r6)
-        self.r7 = QtGui.QRadioButton("1024-2047")
-        self.guess_group.addButton(self.r7)
+        
+        #create range selector buttons        
+        for _range in range(Generator.LOW_POW2, Generator.HIGH_POW2+1):
+            low = 2**_range
+            high = 2*low -1
+            btn_str = str(low)+"-"+str(high) 
+            temp_btn = QtGui.QRadioButton(btn_str)
+            
+            #add the button as a member of the class so it persists
+            member_name = "r"+str(_range)
+            setattr(self, member_name, temp_btn);
+            btn = getattr(self, member_name)
+            self.guess_group.addButton( btn)
+            self.guess_group.setId(btn, _range)
         
         #button to submit estimate
         buttonLoad = QtGui.QPushButton("Load Slide")
@@ -116,14 +115,9 @@ class Generator(QtGui.QWidget):
         num_cells_layout = QtGui.QVBoxLayout()
         num_cells_layout.addWidget(buttonLoad)
         num_cells_layout.addWidget(self.exact_display)
-        num_cells_layout.addWidget(self.r0)
-        num_cells_layout.addWidget(self.r1)
-        num_cells_layout.addWidget(self.r2)
-        num_cells_layout.addWidget(self.r3)
-        num_cells_layout.addWidget(self.r4)
-        num_cells_layout.addWidget(self.r5)
-        num_cells_layout.addWidget(self.r6)
-        num_cells_layout.addWidget(self.r7)
+        buttons = self.guess_group.buttons()
+        for btn in buttons:
+            num_cells_layout.addWidget(btn)
         num_cells_layout.addWidget(buttonGen)
         
         return num_cells_layout
@@ -133,28 +127,14 @@ class Generator(QtGui.QWidget):
         self.exact_display.append('Exact Count: ' + str(self.slide_scene.cell_count))
 
     def genNewSlide(self):
-
-        self.num_cells = 0
-        
-        if self.r0.isChecked():
-            self.num_cells = int(triangular(8,15))
-        elif self.r1.isChecked():
-            self.num_cells = int(triangular(16,31))
-        elif self.r2.isChecked():
-            self.num_cells = int(triangular(32,63))
-        elif self.r3.isChecked():
-            self.num_cells = int(triangular(64,127))
-        elif self.r4.isChecked():
-            self.num_cells = int(triangular(128,255))
-        elif self.r5.isChecked():
-            self.num_cells = int(triangular(256,511))
-        elif self.r6.isChecked():
-            self.num_cells = int(triangular(512,1023))
-        elif self.r7.isChecked():
-            self.num_cells = int(triangular(1024,2047))
-        
-        if (self.num_cells == 0):
+        pow2_range = self.guess_group.checkedId()
+        if (not pow2_range):
             return False
-
+        
+        low = 2**pow2_range
+        high = 2*low -1
+        self.num_cells = int( triangular(low,high))
+        
         self.slide_gen.genSlide(self.slide_scene, self.num_cells)
         self.exact_display.append('Exact Count: ' + str(self.num_cells))
+
