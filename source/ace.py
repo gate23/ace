e2c9299c46bdf87dcaaf41a94b8bdf63e4659623
Andrew Young <younga25@students.wwu.edu>
2014-12-01 20:18:52 -0800

    renamed main to ace

---
+++ source/ace.py
@@ -0,0 +1,20 @@
+"""
+ace.py
+
+Entry point of the application.
+
+"""
+import sys
+from PyQt4 import QtGui
+from mainwindow import MainWindow
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    
+    main_window = MainWindow() 
+    main_window.show()    
+    
+    sys.exit(app.exec_() )
+
+if __name__ == "__main__":
+    main()
